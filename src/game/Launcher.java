a970082f94e9dcf6e216725bdae61d04c1f7428c
monitobeko <monitobeko@abv.bg>
2015-11-05 16:16:18 +0200

    initial commit

---
+++ src/game/Launcher.java
@@ -0,0 +1,8 @@
+package game;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Game game = new Game("Title game!", 1280, 720);
+        game.start();
+    }
+}
---
+++ src/gfx/Assets.java
@@ -0,0 +1,18 @@
+package gfx;
+
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+
+public class Assets {
+
+    private static final int width = 128, height = 128;
+
+    public static BufferedImage player1, player2;
+    //Loads every resource needed for the game
+    public static void init() {
+        SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/java.png"));
+
+        player1 = sheet.crop(0, 0, width, height);
+        player2 = sheet.crop(0, 0, width, height);
+    }
+}
